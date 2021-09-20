62e325d45ea6baae686f415ce7dd7327387974f2
Moses V <moses.valerio321@gmail.com>
2021-09-20 19:48:29 -0400

    Updates to Website Responsivness
    JavaScript
    -added media query to buttons that close and hide overlay when navigating so it wont hide nav bar when over 999px
    
    CSS
    -added website layout for when over 1000px, switched nav bar to a simple top nav
    -changed font sizes
    
    HTML
    -made h1 tag a link to home and got rid of home nav link,
    -added content classes to hopefully streamline css for sections

--- GPD Project/script/script.js
+++ GPD Project/script/script.js
@@ -1,37 +1,24 @@
 /* Opens Nav bar when someone clicks on span element
- makes span element display none when active*/
+makes span element display none when active*/
+const mQuery = window.matchMedia('(max-width: 999px)');
 
-function openNav(){
-   document.getElementById('main-nav').style.width="100%";
-   if(document.getElementById('main-nav').style.width="100%"){
-      document.getElementById('nav-btn').style.display="none";
+function openNav(e){
+      document.getElementById('main-nav').style.width="100%";
+      if(document.getElementById('main-nav').style.width="100%"){
+         document.getElementById('nav-btn').style.display="none";
+      }
    }
-}
+
 
 // close when someone clicks on 'x' symbolinside overlay
-function closeNav(){
-   document.getElementById('main-nav').style.width = "0%";
-   if( document.getElementById('main-nav').style.width = "0%"){
-      document.getElementById('nav-btn').style.display="block";
+if(mQuery.matches){
+   function closeNav(e){ 
+      document.getElementById('main-nav').style.width = "0%";
+      if( document.getElementById('main-nav').style.width = "0%"){
+         document.getElementById('nav-btn').style.display="block";
+      }
    }
 }
 
-// /* Sticky Navbar*/
 
-
-// // get navbar
-// const topNav = document.querySelector('#header');
-
-// // Get the offset position of the navbar
-// let sticky = topNav.offsetTop;
-
-// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function stickyNav() {
-//    if (window.scrollY >= sticky) {
-//       topNav.classList.add('sticky');
-//    } else {
-//       topNav.classList.remove('sticky');
-//    }
-// }
-// // when user scrolls execute function
-// window.addEventListener('scroll',stickyNav);
+mQuery.addEventListener(closeNav);
