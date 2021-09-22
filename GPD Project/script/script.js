851e37b113e3c44daec16fc63fd21634c07c6979
Moses V <moses.valerio321@gmail.com>
2021-09-21 20:15:28 -0400

    Update to Website Home Page
    HTML
    -incorporated a form to send messages
    
    CSS
    -styled the forms for multiple media queries
    
    JS
    -created functions to hide and show the form on the click of the send message button

--- GPD Project/script/script.js
+++ GPD Project/script/script.js
@@ -1,15 +1,13 @@
 /* Opens Nav bar when someone clicks on span element
 makes span element display none when active*/
-const mQuery = window.matchMedia('(max-width: 999px)');
-
 function openNav(e){
       document.getElementById('main-nav').style.width="100%";
       if(document.getElementById('main-nav').style.width="100%"){
          document.getElementById('nav-btn').style.display="none";
       }
    }
-
-
+/*mQuery function will only work if the screen width is true to the values */
+   const mQuery = window.matchMedia('(max-width: 999px)');
 // close when someone clicks on 'x' symbolinside overlay
 if(mQuery.matches){
    function closeNav(e){ 
@@ -19,6 +17,22 @@ if(mQuery.matches){
       }
    }
 }
-
-
 mQuery.addEventListener(closeNav);
+
+// Opens form when pressing the send message button
+
+function openForm(){
+   document.querySelector('.popup-form').style.display="block";
+   if(document.querySelector('.popup-form').style.display="block"){
+      document.querySelector('#vf-btn').style.display = "none";
+      document.querySelector('.cancel').style.display = "block";
+   }
+}
+// closes for when pressed
+function closeForm(){
+   document.querySelector('.popup-form').style.display="none";
+   if(document.querySelector('.popup-form').style.display="none"){
+      document.querySelector('#vf-btn').style.display = "block";
+      document.querySelector('.cancel').style.display = "none";
+   }
+}
